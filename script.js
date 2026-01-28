a5ed37174ac645552ba7713fe630078d6848916f
byteRaj-commits <rajp182138@gmail.com>
2026-01-28 07:21:41 +0530

    make changes in html and css file and update the code of js

--- script.js
+++ script.js
@@ -11,14 +11,32 @@ document.addEventListener("DOMContentLoaded", function(){
     const temperature = document.querySelector(".temp");
     const humidityDetail = document.querySelector(".humidity");
     const winDSpeed = document.querySelector(".wind");
+    const weatherCard = document.querySelector(".weather")
+    const weatherIcon = document.querySelector(".weather-icon");
+    const errorMsg = document.querySelector(".error");
 
 
 
     async function checkWeather(city){
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+         try{
+
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            if (!response.ok) {   // handles 404, 401, 500 etc.
+                throw new Error("City not found");
+            }
+            const data = await response.json();
+            displayCityData(data);
+
+            weatherCard.classList.remove("hidden");
+            errorMsg.style.display = "none";
+        }
+        catch(error){
+            console.error(error);
+            errorMsg.style.display = "block";
+            weatherCard.classList.add("hidden");
+            errorMsg.classList.remove("hidden");
+        }
        
-        const data = await response.json();
-        displayCityData(data);
     }
 
     function displayCityData(data){
@@ -26,7 +44,19 @@ document.addEventListener("DOMContentLoaded", function(){
         temperature.innerHTML = Math.round(data.main.temp) + "°C";
         humidityDetail.innerHTML = data.main.humidity + " %";
         winDSpeed.innerHTML = data.wind.speed + " km/h";
+
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "Assets/clouds.png";
+        }else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "Assets/clear.png";
+        }else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "Assets/rain.png";
+        }else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "Assets/drizzle.png";
+        }else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "Assets/mist.png";
         }
+    }
 
     searchButton.addEventListener("click", ()=>{
         checkWeather(searchInput.value);
